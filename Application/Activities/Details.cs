0bcb132409803fed14abcad98864de81f46c67b0
evgiv0 <zhe956@gmail.com>
2022-01-02 14:13:43 +0300

    Add exception handling logic

--- Application/Activities/Details.cs
+++ Application/Activities/Details.cs
@@ -1,8 +1,10 @@
+using System.Security.AccessControl;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Core;
 using Domain;
 using MediatR;
 using Persistence;
@@ -11,12 +13,12 @@ namespace Application.Activities
 {
     public class Details
     {
-        public class Query : IRequest<Activity>
+        public class Query : IRequest<Result<Activity>>
         {
             public Guid Id { get; set; }
         }
 
-        public class Handler : IRequestHandler<Query, Activity>
+        public class Handler : IRequestHandler<Query, Result<Activity>>
         {
             private readonly DataContext _context;
             public Handler(DataContext context)
@@ -24,9 +26,14 @@ namespace Application.Activities
                 _context = context;
             }
 
-            public async Task<Activity> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<Result<Activity>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _context.Activities.FindAsync(request.Id);
+                var activity = await _context.Activities.FindAsync(request.Id);
+
+                if (activity == null)
+                    return Result<Activity>.Failure("Activity is not found");
+    
+                return Result<Activity>.Success(activity);
             }
         }
     }
